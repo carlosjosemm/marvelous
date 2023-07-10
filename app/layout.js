e62bc1bc98e495b45a07a61000cc41b072d76c4b
Carlos José Moncada <carlosjmoncadam@gmail.com>
2023-07-10 18:27:41 -0300

    feat: migrate to App routing from next.js pages

---
+++ app/layout.js
@@ -0,0 +1,9 @@
+const layout = ({children}) => {
+    return ( 
+        <html lang="en">
+            <body>{children}</body>
+        </html>
+    );
+}
+ 
+export default layout;
---
+++ app/page.js
@@ -0,0 +1,7 @@
+const Page = () => {
+    return ( 
+        <h1>Fist page from App</h1>
+     );
+}
+ 
+export default Page;

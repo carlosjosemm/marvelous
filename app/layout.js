987d86185903ccaf78eb6490820f353ded0270ad
Carlos José Moncada <carlosjmoncadam@gmail.com>
2023-07-12 20:18:55 -0300

    feat: moved header and footer back to layout, all page now on client-side. SSR not compatible with styled components.

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,19 @@
+'use client'
 import styles from '../styles/globals.css'
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 
 const layout = ({children}) => {
     return ( 
         <html lang="en">
             <body>
-                {children}
+                <div style={{display: "flex", flexDirection: 'column', minHeight: '100vh'}}>
+                <Header />
+                    <main style={{flexGrow: '2', minHeight: '70vh'}}>
+                        {children}
+                    </main>
+                <Footer />
+                </div>
             </body>
         </html>
     );
--- app/page.js
+++ app/page.js
@@ -1,16 +1,6 @@
-'use client'
-import Footer from "../components/Footer";
-import Header from "../components/Header";
-
 const Page = () => {
     return ( 
-        <div style={{display: "flex", flexDirection: 'column', minHeight: '100vh'}}>
-            <Header />
-                <main style={{flexGrow: '2', minHeight: '70vh'}}>
-                    <h1>Fist page from App</h1>
-                </main>
-            <Footer />
-        </div>
+        <h1>Home page</h1>
      );
 }
  

f12284b76e42f8240b9e1da0f614238c1392a2c9
Carlos José Moncada <carlosjmoncadam@gmail.com>
2023-07-14 14:28:08 -0300

    feat: added styled-components theme provider

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 'use client'
+import Theme from "./Theme";
 import styles from '../styles/globals.css'
 import Footer from "../components/Footer";
 import Header from "../components/Header";
@@ -7,13 +8,15 @@ const layout = ({children}) => {
     return ( 
         <html lang="en">
             <body>
-                <div style={{display: "flex", flexDirection: 'column', minHeight: '100vh'}}>
-                <Header />
-                    <main style={{flexGrow: '2', minHeight: '70vh'}}>
-                        {children}
-                    </main>
-                <Footer />
-                </div>
+                <Theme>
+                    <div style={{display: "flex", flexDirection: 'column', minHeight: '100vh'}}>
+                    <Header />
+                        <main style={{flexGrow: '2', minHeight: '70vh'}}>
+                            {children}
+                        </main>
+                    <Footer />
+                    </div>
+                </Theme>
             </body>
         </html>
     );

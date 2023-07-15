2a86c020aaea65a67629f2d8a706c59e2ebd1111
Carlos José Moncada <carlosjmoncadam@gmail.com>
2023-07-15 12:35:06 -0300

    fix: added a styled components registry to fix hydration issue with injected styles.

--- app/layout.js
+++ app/layout.js
@@ -1,22 +1,25 @@
 'use client'
 import Theme from "./Theme";
-import styles from '../styles/globals.css'
-import Footer from "../components/Footer";
-import Header from "../components/Header";
+import '../styles/globals.css'
+import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Header";
+import StyledComponentsRegistry from "../lib/registry";
 
 const layout = ({children}) => {
     return ( 
         <html lang="en">
             <body>
-                <Theme>
-                    <div style={{display: "flex", flexDirection: 'column', minHeight: '100vh'}}>
-                    <Header />
-                        <main style={{flexGrow: '2', minHeight: '70vh'}}>
-                            {children}
-                        </main>
-                    <Footer />
-                    </div>
-                </Theme>
+                <StyledComponentsRegistry>
+                    <Theme>
+                        <div style={{display: "flex", flexDirection: 'column', minHeight: '100vh'}}>
+                        <Header />
+                            <main style={{flexGrow: '2', minHeight: '70vh'}}>
+                                {children}
+                            </main>
+                        <Footer />
+                        </div>
+                    </Theme>
+                </StyledComponentsRegistry>
             </body>
         </html>
     );

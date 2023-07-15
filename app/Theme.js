564275b8aeb94264f3cf62748f114490fc02102d
Carlos José Moncada <carlosjmoncadam@gmail.com>
2023-07-15 12:37:40 -0300

    feat: added breakpoints on the theme to be globally accesible.

--- app/Theme.js
+++ app/Theme.js
@@ -2,6 +2,13 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 
 const theme = {
+  breakpoints: {
+    mobileSmall: '320px',
+    mobileLarge: '640px',
+    tablet: '768px',
+    laptop: '1024px',
+    laptopLarge: '1280px'
+  },
   colors: {
     powderWhite: "#FFFDF9",
     persianGreen: "#06B49A",

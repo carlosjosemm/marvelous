a21f1a81b3c9e2252f2c32a5871cbb6b43cbf75c
Carlos José Moncada <carlosjmoncadam@gmail.com>
2023-07-17 17:17:56 -0300

    fix: fixed the responsive sizes order. They were originally reversed.

--- components/Hero/HeroThumbnail.jsx
+++ components/Hero/HeroThumbnail.jsx
@@ -1,22 +1,20 @@
 import styled from 'styled-components'
 
 const HeroThumbnail = styled.img`
-    @media only screen and (max-width: ${props => props.theme.breakpoints.mobileSmall}) {
-        max-width: 180px;
-    }
-    @media only screen and (max-width: ${props => props.theme.breakpoints.mobileLarge}) {
-        max-width: 200px;
-    }
-    @media only screen and (max-width: ${props => props.theme.breakpoints.tablet}) {
-        max-width: 280px;
+    @media only screen and (min-width: ${props => props.theme.breakpoints.laptop}) {
+        max-width: 450px;
     }
     @media only screen and (max-width: ${props => props.theme.breakpoints.laptop}) {
         max-width: 380px;
     }
-    @media only screen and (min-width: ${props => props.theme.breakpoints.laptop}) {
-        max-width: 450px;
+    @media only screen and (max-width: ${props => props.theme.breakpoints.tablet}) {
+        max-width: 280px;
     }
-
-
+    @media only screen and (max-width: ${props => props.theme.breakpoints.mobileLarge}) {
+        max-width: 200px;
+    }
+    @media only screen and (max-width: ${props => props.theme.breakpoints.mobileSmall}) {
+        max-width: 180px;
+    }
 `
 export default HeroThumbnail

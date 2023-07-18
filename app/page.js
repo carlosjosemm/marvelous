63a0f77f98e378fc19bde36e0e6b150d12474309
Carlos José Moncada <carlosjmoncadam@gmail.com>
2023-07-18 18:48:40 -0300

    feat: making the hero page the default page for the user.

--- app/page.js
+++ app/page.js
@@ -1,6 +1,13 @@
+import { redirect } from "next/navigation";
+
 const Page = () => {
+    /**
+       Directly re-directing to the Hero Page as per the requirements of the project.
+       Could be expanded to include a more generic landing page.
+    **/
+    redirect('/hero')
     return ( 
-        <h1>Home page</h1>
+        <></>
      );
 }
  

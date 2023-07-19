5b9529f07587a7cccd8bb2abb87bba5f3dfaa75e
Carlos José Moncada <carlosjmoncadam@gmail.com>
2023-07-19 14:08:31 -0300

    feat: added a loading state for the results area of search page.

--- app/search/page.js
+++ app/search/page.js
@@ -1,12 +1,20 @@
 'use client'
 import { useSearchParams } from "next/navigation";
+import { useState } from "react";
 
 const SearchPage = () => {
+    const [loading, setLoading] = useState(true);
+
     const searchParams = useSearchParams()
     const getSearchMatrix = () => {
         const hero = searchParams.getAll('hero');
         const comic = searchParams.getAll('comic');
     }
+
+    if (loading) {
+        return <h1>Loading search results...</h1>
+    }
+
     return (
         <h1>Search results here...</h1>
      );

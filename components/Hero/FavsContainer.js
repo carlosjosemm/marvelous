6ad6dd96a7941cc88f24771b89c63fcba765b7f4
Carlos José Moncada <carlosjmoncadam@gmail.com>
2023-07-23 13:50:04 -0300

    feat: add a container to display grid of favorites in the form of hero cards

---
+++ components/Hero/FavsContainer.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import HeroContainer from "./HeroContainer";
+import ContentPreview from "./ContentPreview";
+
+const StyledFavsContainer = styled(HeroContainer)`
+    grid-template-columns: repeat(4, 1fr);
+    justify-self: stretch;
+    align-self: stretch;
+`
+const StyledTitle = styled.h3`
+    grid-column: span 4;
+`
+/** 
+ * @typedef {object} props
+ * @property {Array<{contentData: {}, secSearchParam: Array}>} content
+ */
+const FavsContainer = (/** @type {props}*/ {content}) => {
+    return ( 
+        <StyledFavsContainer>
+            <StyledTitle>Favorites</StyledTitle>
+            {content.map((fav, index) => {
+                return (
+                    <ContentPreview contentData={fav.contentData} secSearchParam={fav.secSearchParam} key={index*1000*Math.random()}/>
+                )
+            })}
+        </StyledFavsContainer> 
+    );
+}
+ 
+export default FavsContainer;

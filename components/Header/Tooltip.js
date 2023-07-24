4af2d6ffd3416161dfa7a90d63230042dff598b5
Carlos José Moncada <carlosjmoncadam@gmail.com>
2023-07-24 14:45:50 -0300

    feat: created a tooltip component with variable position.

---
+++ components/Header/Tooltip.js
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+
+const StyledTooltipContent = styled.span`
+    visibility: hidden;
+    position: absolute;
+    ${props => props.$position == 'bottom' ? 'top: calc(100% + 5px);' : ''}
+    ${props => props.$position == 'bottom' ? 'margin-left: -2rem;' : ''}
+    border-radius: 4px;
+    font-size: 1rem;
+    transition: display 0.3s, background-color 0.3s, width 0.3s, padding 0.3s ease-in-out;
+    &:before {
+      content: "";
+      width: 0;
+      height: 0;
+      ${props => props.$position == 'bottom' ? 'left: 1rem;' : 'left: -4px;'}
+      ${props => props.$position == 'bottom' ? 'top: -4px;' : 'top: 10px;'}
+      position: absolute;
+      border: 5px solid transparent;
+      ${props => props.$position == 'bottom' ? 'transform: rotate(135deg);' : 'transform: rotate(45deg);'}
+      transition: border 0.4s ease-in-out;
+   }
+`
+const StyledTooltipTrigger = styled.span`
+
+`
+const StyledTooltipWrapper = styled.span`
+    cursor: default;
+    position: relative;
+    & ${StyledTooltipTrigger}:hover + ${StyledTooltipContent} {
+        visibility: visible;
+        color: #fff;
+        background-color: rgba(0, 0, 0, 0.7);
+        width: max-content;
+        padding: 8px 8px;
+        &:before {
+         border-color: transparent transparent rgba(0, 0, 0, 0.7) rgba(0, 0, 0, 0.7);
+        }
+   }
+`
+
+const Tooltip = ({children, tooltipContent, position}) => {
+    return ( 
+        <StyledTooltipWrapper>
+            <StyledTooltipTrigger>
+                {children}
+            </StyledTooltipTrigger>
+            <StyledTooltipContent $position={position}>
+                {tooltipContent}
+            </StyledTooltipContent>
+        </StyledTooltipWrapper>
+     );
+}
+ 
+export default Tooltip;
